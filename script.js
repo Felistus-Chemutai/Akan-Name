3236feb9d6dbecf217cc201b3a7a4d443670a2e6
Felistus Chemutai <chemunicole@gmail.com>
2020-10-06 00:17:12 +0300

    Index Changes

---
+++ script.js
@@ -0,0 +1,41 @@
+function day (cc,yy,mm,dd){
+    return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+  }
+  function myClick(){
+    var year = document.getElementById("year").value;
+    var Month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var akan = new Date();
+    akan.setDate(parseInt(day));
+    akan.setMonth(parseInt(Month)-1);
+    akan.setFullYear(parseInt(year));
+    var c = akan.getDay();
+    if(c===0){
+        alert("Sunday");
+    }else if(c===1){
+        alert("Monday");
+    }else if(c===2){
+        alert("Tuesday");
+    }else if(c===3){
+        alert("Wednesday");
+    }else if(c===4){
+        alert("Thursday");
+    }else if(c===5){
+        alert("Friday");
+    }else if(c===6){
+       alert("Saturday");
+    }
+    if(year==""||year>2030){
+        alert("insert a valid year");
+    }else if(Month=="" ||Month>12){
+        alert("Enter a valid month");
+    }else if(day=="" ||day>31){
+        alert("Enter a valid date");
+    }else if(document.getElementById("male").checked===true && document.getElementById("female").checked==false){
+        alert("Your Akan name is " + maleName[c]);
+    }else if(document.getElementById("male").checked===false && document.getElementById("female").checked==true){
+        alert("Your Akan name is " + femaleName[c]);
+    }
+}
